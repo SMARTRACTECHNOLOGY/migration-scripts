49228f3308c19dfa9c5eb0780e31bb18d709c3f8
TrunKumar <trun.kumar@innobitsystems.com>
2023-04-10 13:04:55 +0530

    Updated Factory Tag Script

--- factory-tag-migration-03-03-2023.js
+++ factory-tag-migration-03-03-2023.js
@@ -55,7 +55,7 @@ async function getCustomerHashMap(connection, batchId) {
             orderDate: data?.orderDate,
             orderQuantity: data?.orderQuantity,
             orderQuantityUnit: data?.orderQuantityUnit,
-           
+
         }
         customerHashMap.set(batchId, batchObject);
 
@@ -72,94 +72,119 @@ async function readTagData(currentProdConnection, newProdConnection) {
     let chunkSize01 = 1000;
     let chunkSize02 = 500;
     let chunkSize03 = 100;
+    let fetchLimit=1000;
+    let fetchLimitSkip=0;
     // getting distinct batch id
-    let batchData_ = await currentProdConnection.collection('Tag').distinct("bId");
-    console.log("batchData_", batchData_, new Date());
-    for (let i = 0; i < batchData_.length; i++) {
-        console.log("New Batch ID :", batchData_[i]);
-        let bulkData = [];
-        let offset = 0;
-        //calling batchId HashMap
-        let custumerData_ = await getCustomerHashMap(currentProdConnection, batchData_[i])
-        let custumerData = custumerData_.get(batchData_[i])
-        console.log(custumerData);
+    //let batchData_ = await currentProdConnection.collection('Tag').distinct("bId");
 
-        while (true) {
-            
-            let localCounter = 0;
-            let data_ = await currentProdConnection.collection('Tag').find({ "bId": batchData_[i] }).sort({ ct: 1 }).skip(offset).limit(limit).toArray();
-            console.log("Total Record Found For BatchID=>", batchData_[i], "Using Offest ", offset, "Data Found==>", data_.length)
-            if (data_.length === 0) {
-                break;
+    while (true) {
+
+        let batchData_ = await currentProdConnection.collection('Tag').aggregate([
+            { $sort: { "bId": 1 } },
+            { $project: { "bId": 1 } },
+            { $group: { "_id": "$bId" } },
+            { $skip: fetchLimitSkip },
+            { $limit: fetchLimit }
+        ]).toArray();
+       if(batchData_.length==0)
+       {
+        break;
+       } 
+        console.log("batchData_", batchData_.length, new Date());
+        for (let i = 0; i < batchData_.length; i++) {
+            let batchID = batchData_[i]._id;
+           // checking batch id is exist in new collection or not  
+           let checkExistingBatchId=await newProdConnection.collection('unassignedtagsdatas').find({batchId: batchID}).toArray();
+           if(checkExistingBatchId.length>0)
+            {
+                continue;
+            }       
+
+
+            console.log("New Batch ID :", batchID);
+            let bulkData = [];
+            let offset = 0;
+            //calling batchId HashMap
+            let custumerData_ = await getCustomerHashMap(currentProdConnection, batchID)
+            let custumerData = custumerData_.get(batchID)
+            console.log(custumerData);
+
+            while (true) {
+
+                let localCounter = 0;
+                let data_ = await currentProdConnection.collection('Tag').find({ "bId": batchID }).sort({ ct: 1 }).skip(offset).limit(limit).toArray();
+                console.log("Total Record Found For BatchID=>", batchID, "Using Offest ", offset, "Data Found==>", data_.length)
+                if (data_.length === 0) {
+                    break;
+                }
+                for (const data of data_) {
+
+                    const now = new Date();
+                    const utcDatetime = now.toISOString().slice(0, 19) + '.000+00:00';
+
+                    let final_data =
+                    {
+                        batchId: batchID,
+                        tagId: data.htId,
+                        secureKey: data.skey,
+                        src: data.src,
+                        customerName: custumerData?.customerName,
+                        deliveryDate: custumerData?.deliveryDate,
+                        deliveryItemName: custumerData?.deliveryItemName,
+                        deliveryQuantity: custumerData?.deliveryQuantity,
+                        deliveryQuantityUnit: custumerData?.deliveryQuantityUnit,
+                        historyReferenceId: custumerData?.historyReferenceId,
+                        orderId: custumerData?.orderId,
+                        orderDate: custumerData?.orderDate,
+                        orderQuantity: custumerData?.orderQuantity,
+                        orderQuantityUnit: custumerData?.orderQuantityUnit,
+                        createdAt: new Date(utcDatetime),
+                    }
+
+                    //  5100
+                    // 0 < 500 && 5100 - 5000 > 500
+                    if (bulkData.length < chunkSize01 && data_.length - localCounter > chunkSize01) {
+                        bulkData.push(final_data);
+                        counter++;
+                        localCounter++
+                        continue;
+
+                    }
+                    if (bulkData.length < chunkSize02 && data_.length - localCounter > chunkSize02) {
+                        bulkData.push(final_data);
+                        counter++;
+                        localCounter++
+                        continue;
+
+                    }
+                    if (bulkData.length < chunkSize03 && data_.length - localCounter > chunkSize03) {
+                        bulkData.push(final_data);
+                        counter++;
+                        localCounter++
+                        continue;
+
+                    }
+
+
+                    bulkData.push(final_data);
+                    // insert into new production DB
+                    await newProdConnection.collection('unassignedtagsdatas').insertMany(bulkData);
+                    bulkData = [];
+                    counter++;
+                    localCounter++
+                    console.log(bulkData.length, "<", chunkSize03, "&&", data_.length, "-", localCounter, ">", chunkSize03)
+                    await delay(500);
+
+                }
+
+                offset = offset + data_.length;
+                console.log("Total Inserted Records For Batch Id=> ", batchID, "LocalCounter===>", localCounter, " Total Inserted Records==>", counter, new Date());
+                await delay(1000);
+
             }
-            for (const data of data_) {
-
-                const now = new Date();
-                const utcDatetime = now.toISOString().slice(0, 19) + '.000+00:00';
-
-                let final_data =
-                {
-                    batchId: batchData_[i],
-                    tagId: data.htId,
-                    secureKey: data.skey,
-                    src: data.src,
-                    customerName: custumerData?.customerName,
-                    deliveryDate: custumerData?.deliveryDate,
-                    deliveryItemName: custumerData?.deliveryItemName,
-                    deliveryQuantity: custumerData?.deliveryQuantity,
-                    deliveryQuantityUnit: custumerData?.deliveryQuantityUnit,
-                    historyReferenceId: custumerData?.historyReferenceId,
-                    orderId: custumerData?.orderId,
-                    orderDate: custumerData?.orderDate,
-                    orderQuantity: custumerData?.orderQuantity,
-                    orderQuantityUnit: custumerData?.orderQuantityUnit,
-                    createdAt:new Date(utcDatetime),
-                }
-
-                //  5100
-                // 0 < 500 && 5100 - 5000 > 500
-                if (bulkData.length < chunkSize01 && data_.length - localCounter > chunkSize01) {
-                    bulkData.push(final_data);
-                    counter++;
-                    localCounter++
-                    continue;
-
-                }
-                if (bulkData.length < chunkSize02 && data_.length - localCounter > chunkSize02) {
-                    bulkData.push(final_data);
-                    counter++;
-                    localCounter++
-                    continue;
-
-                }
-                if (bulkData.length < chunkSize03 && data_.length - localCounter > chunkSize03) {
-                    bulkData.push(final_data);
-                    counter++;
-                    localCounter++
-                    continue;
-
-                }
-
-
-                bulkData.push(final_data);
-                // insert into new production DB
-                await newProdConnection.collection('unassignedtagsdatas').insertMany(bulkData);
-                bulkData = [];
-                counter++;
-                localCounter++
-                console.log(bulkData.length ,"<", chunkSize03 ,"&&", data_.length ,"-", localCounter ,">", chunkSize03)
-                await delay(500);
-                
-            }
-            
-            offset = offset + data_.length;
-            console.log("Total Inserted Records For Batch Id=> ", batchData_[i], "LocalCounter===>", localCounter, " Total Inserted Records==>", counter, new Date());
-            await delay(1000);
-
         }
+        fetchLimitSkip=fetchLimitSkip+batchData_.length;
     }
-
-
 }
 
 
